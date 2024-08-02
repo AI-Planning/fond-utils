738b9ee8fbeabc3a8866b07ebe9a8df9138ea38c
Christian Muise <christian.muise@gmail.com>
2024-08-02 14:33:43 -0400

    First pass. Seems to work for most domains (13 fail for a small handful of reasons)

--- determizer.py
+++ determizer.py
@@ -2,19 +2,57 @@
 import argparse
 
 from pddl import parse_domain
+from pddl.action import Action
+from pddl.core import Domain
+from pddl.formatter import domain_to_string
+from pddl.logic.base import OneOf
+from pddl.logic.effects import AndEffect
+
+from normalizer import normalize
+
+
+def determinize(domain):
+    
+    new_actions = []
+
+    for act in domain.actions:
+        new_act = normalize(act)
+        if isinstance(new_act.effect, OneOf):
+            counter = 1
+            for eff in new_act.effect.operands:
+                assert isinstance(eff, AndEffect), f"Effect in OneOf is not an AndEffect: {eff}"
+                new_actions.append(Action(
+                    name=f"{act.name}_DETDUP_{counter}",
+                    parameters=act.parameters,
+                    precondition=act.precondition,
+                    effect=eff)
+                )
+                counter += 1
+        else:
+            new_actions.append(new_act)
+
+    return Domain(
+        name=domain.name,
+        requirements=domain.requirements,
+        types=domain.types,
+        constants=domain.constants,
+        predicates=domain.predicates,
+        actions=new_actions,
+        functions=domain.functions,
+        derived_predicates=domain.derived_predicates)
+
 
 
 def main(din, dout):
     domain = parse_domain(din)
+    detdomain = determinize(domain)
+    with open(dout, 'w') as f:
+        f.write(domain_to_string(detdomain))
 
-    print(domain.actions)
-    # domain.determinize()
-    # domain.write(dout)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # --input and --output
-    parser.add_argument('--input', dest='din', required=True)
-    parser.add_argument('--output', dest='dout', required=True)
+    parser.add_argument('--input', dest='din', required=True, help='Input (non-deterministic) domain file')
+    parser.add_argument('--output', dest='dout', required=True, help='Output (deterministic) domain file')
     args = parser.parse_args()
     main(args.din, args.dout)
---
+++ normalizer.py
@@ -0,0 +1,84 @@
+
+from pddl.action import Action
+from pddl.logic.base import OneOf, Not
+from pddl.logic.effects import When, AndEffect
+from pddl.logic.predicates import Predicate
+
+from itertools import product, chain
+
+DEBUG = False
+
+def normalize(op):
+
+    effs = flatten(op.effect)
+
+    if len(effs) == 1:
+        eff = effs[0]
+    else:
+        # Normalize to wrap every operand of an OneOf in an AndEffect
+        for i in range(len(effs)):
+            if not isinstance(effs[i], AndEffect):
+                effs[i] = AndEffect(effs[i])
+        
+        # As an optimization, compress one level of nested AndEffects on the outcomes
+        new_outcomes = []
+        for outcome in effs:
+            if isinstance(outcome, AndEffect):
+                new_operands = []
+                for operand in outcome.operands:
+                    if isinstance(operand, AndEffect):
+                        new_operands.extend(operand.operands)
+                    else:
+                        new_operands.append(operand)
+                new_outcomes.append(AndEffect(*new_operands))
+            else:
+                new_outcomes.append(outcome)
+
+        eff = OneOf(*new_outcomes)
+
+    return Action(
+        name=op.name,
+        parameters=op.parameters,
+        precondition=op.precondition,
+        effect=eff)
+
+def flatten(eff):
+    return _flatten(eff)
+
+def combine(eff_lists):
+    combos = list(product(*eff_lists))
+    combined_oneofs = [AndEffect(*[x for x in choice if x != AndEffect([])]) for choice in combos]
+    if DEBUG:
+        print ("\nCombining:\n%s" % '\n'.join(map(str, eff_lists)))
+        print ("Result: %s\n" % combined_oneofs)
+    return combined_oneofs
+
+def _flatten(eff):
+
+    if DEBUG:
+        print ("Flattening %s" % str(eff))
+
+    if isinstance(eff, AndEffect):
+        if 0 == len(eff.operands):
+            return [eff]
+        else:
+            return combine(list(map(_flatten, eff.operands)))
+
+    elif isinstance(eff, OneOf):
+        return list(chain(*(list(map(_flatten, eff.operands)))))
+
+    elif isinstance(eff, When):
+        return [When(eff.condition, res) for res in _flatten(eff.result)]
+
+    # Default cases
+    elif isinstance(eff, Not):
+        return [eff]
+
+    elif isinstance(eff, Predicate):
+        return [eff]
+    
+    else:
+        if DEBUG:
+            print ("Base: %s" % str(eff))
+        raise ValueError("Unexpected effect type: %s" % type(eff))
+        # return [eff]
