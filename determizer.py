8dd6785981e54a24d66636e42555e4d0e955792d
ssardina <ssardina@gmail.com>
2024-08-16 09:44:25 +1000

    cleaner update of requirements on determinization

--- determizer.py
+++ determizer.py
@@ -1,4 +1,3 @@
-
 import argparse
 
 from pddl import parse_domain
@@ -7,13 +6,15 @@ from pddl.core import Domain
 from pddl.formatter import domain_to_string
 from pddl.logic.base import OneOf
 from pddl.logic.effects import AndEffect
+from pddl.requirements import Requirements
+
 
 from normalizer import normalize
 
 DEBUG = False
 
-def determinize(domain):
-    
+
+def determinize(domain: Domain) -> Domain:
     new_actions = []
 
     for act in domain.actions:
@@ -25,12 +26,16 @@ def determinize(domain):
         if isinstance(new_act.effect, OneOf):
             counter = 1
             for eff in new_act.effect.operands:
-                assert isinstance(eff, AndEffect), f"Effect in OneOf is not an AndEffect: {eff}"
-                new_actions.append(Action(
-                    name=f"{act.name}_DETDUP_{counter}",
-                    parameters=act.parameters,
-                    precondition=act.precondition,
-                    effect=eff)
+                assert isinstance(
+                    eff, AndEffect
+                ), f"Effect in OneOf is not an AndEffect: {eff}"
+                new_actions.append(
+                    Action(
+                        name=f"{act.name}_DETDUP_{counter}",
+                        parameters=act.parameters,
+                        precondition=act.precondition,
+                        effect=eff,
+                    )
                 )
                 counter += 1
         else:
@@ -38,26 +43,38 @@ def determinize(domain):
 
     return Domain(
         name=domain.name,
-        requirements=domain.requirements,
+        requirements=frozenset(
+            [r for r in domain.requirements if r is not Requirements.NON_DETERMINISTIC]
+        ),
         types=domain.types,
         constants=domain.constants,
         predicates=domain.predicates,
         actions=new_actions,
         functions=domain.functions,
-        derived_predicates=domain.derived_predicates)
-
+        derived_predicates=domain.derived_predicates,
+    )
 
 
 def main(din, dout):
     domain = parse_domain(din)
     detdomain = determinize(domain)
-    with open(dout, 'w') as f:
-        f.write(domain_to_string(detdomain).replace(':non-deterministic', ''))
+    with open(dout, "w") as f:
+        f.write(domain_to_string(detdomain))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', dest='din', required=True, help='Input (non-deterministic) domain file')
-    parser.add_argument('--output', dest='dout', required=True, help='Output (deterministic) domain file')
+    parser.add_argument(
+        "--input",
+        dest="din",
+        required=True,
+        help="Input (non-deterministic) domain file",
+    )
+    parser.add_argument(
+        "--output",
+        dest="dout",
+        required=True,
+        help="Output (deterministic) domain file",
+    )
     args = parser.parse_args()
     main(args.din, args.dout)
